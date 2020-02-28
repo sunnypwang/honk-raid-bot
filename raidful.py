d88577cae994b68924a6c607736c4c403351bbe9
Phakawat Wangkriangkri <mariosunshinekirby@gmail.com>
2020-02-29 01:04:20 +0700

    implemented name snapping feature

--- raidful.py
+++ raidful.py
@@ -22,30 +22,30 @@ def getAllRaid():
 
 
 def getAllRaidbyOwner(owner):
-    #new code using mongoquery
-    #not working for now
-    #prevent URL injection maybe?
-    #if '"' in owner:
+    # new code using mongoquery
+    # not working for now
+    # prevent URL injection maybe?
+    # if '"' in owner:
     #    return []
-    
+
     #r = requests.get(url=const.SHEET_URL, params={"owner",owner})
-    #print(r.url)
+    # print(r.url)
     #data = r.json()
 
-    #print(data)
-    
+    # print(data)
+
     raids = getAllRaid()
     result = []
     for raid in raids:
         if raid['owner'] == owner:
             result.append(raid)
-    
+
     return result
 
 
 def getRaidbyID(id):
     r = requests.get(url=const.SHEET_URL + '/' + id.upper())
-    #print(r.url)
+    # print(r.url)
     data = r.json()
     return data
 
@@ -81,7 +81,8 @@ def postRaid(params, owner):
     if len(params) == 0:
         return 'Please provide Pokemon name (If Gigantamax please include G or GMax)'
 
-    pokemon = params[0]
+    pokemon = util.getClosestMatch(params[0])
+
     rarity = 5
     gmax = False
     for param in params:
@@ -124,10 +125,10 @@ def openRaid(params, raid_start, owner):
 
     # OLD CODE BELOW
 
-    raids = getAllRaidbyOwner(owner) #get the raid list from the owner
+    raids = getAllRaidbyOwner(owner)  # get the raid list from the owner
     raid_id = None
 
-    specified_id = True #to keep track if the raid ID is specified or not
+    specified_id = True  # to keep track if the raid ID is specified or not
 
     # if the raid is already started
     if raid_start:
@@ -136,8 +137,8 @@ def openRaid(params, raid_start, owner):
     # if the id is not specified
     if len(params) <= 1:
         if len(raids) > 1:
-            #if no parameter are provided or the first one is passcode
-            if len(params)==0 or params[0].isdigit():
+            # if no parameter are provided or the first one is passcode
+            if len(params) == 0 or params[0].isdigit():
                 return {'status': 'error', 'msg': 'Please provide raid ID (and optionally 4-digit passcode)'}
             else:
                 raid_id = params[0].upper()
@@ -149,22 +150,21 @@ def openRaid(params, raid_start, owner):
     else:
         raid_id = params[0].upper()
 
-    
     code = '-'
 
     if len(params) > 1 or not specified_id:
-        #check if any params has been provided or not
-        if len(params) !=0:
-            #if provided parmeter, check if it's correct or not
+        # check if any params has been provided or not
+        if len(params) != 0:
+            # if provided parmeter, check if it's correct or not
             if specified_id:
                 code = params[1]
 
-                #Check if the code is in correct format
+                # Check if the code is in correct format
                 if not code.isdigit() or len(code) != 4:
                     return {'status': 'error', 'msg': 'Passcode must be 4-digit'}
             else:
                 code = params[0]
-                #Check if the code is in correct format.revert to none if it's not
+                # Check if the code is in correct format.revert to none if it's not
                 if not code.isdigit() or len(code) != 4:
                     code = '-'
                     raid_id = params[0]
@@ -174,16 +174,15 @@ def openRaid(params, raid_start, owner):
     if 'error' in raid.keys():
         return {'status': 'error', 'msg': 'ID not found. Please type `!list` for all available raids'}
 
-    if raid['owner']!=owner:
+    if raid['owner'] != owner:
         return {'status': 'error', 'msg': 'This raid belongs to another trainer!'}
-
 
     raid['opened'] = True
     raid['code'] = code
     put_url = f'{const.SHEET_URL}/{id}&method=PUT'
     r = requests.post(url=put_url, json=raid)
 
-    return {'status': 'ok', 'embed': util.embedRaid(raid),'raid':raid}
+    return {'status': 'ok', 'embed': util.embedRaid(raid), 'raid': raid}
 
 
 def closeRaid():
